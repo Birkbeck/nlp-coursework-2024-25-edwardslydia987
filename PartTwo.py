7bdbdfa8fa19e392057f86757fb26ea7c4adc313
edwardslydia987 <edwardslydia987@gmail.com>
2025-06-15 16:47:28 +0000

    changing format to functions for Q2a

--- PartTwo.py
+++ PartTwo.py
@@ -2,23 +2,29 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('hansard40000.csv')
-df['party']  = df['party'].replace('Labour (Co-op)', 'Labour')
-df = df[df.party != 'Speaker']
+def subset_and_rename(file):
+    df = pd.read_csv(file)
+    df['party']  = df['party'].replace('Labour (Co-op)', 'Labour')
+    df = df[df.party != 'Speaker']
+    
+    common_party = df['party'].value_counts().nlargest(4)
+    df = df[df['party'].isin(common_party.index)]
 
-common_party = df['party'].value_counts().nlargest(4)
-df = df[df['party'].isin(common_party.index)]
+    df = df[df.speech_class == 'Speech']
 
-df = df[df.speech_class == 'Speech']
+    df = df[df['speech'].str.len() < 1000]
 
-df = df[df['speech'].str.len() < 1000]
-
-
-print(df.shape)
+    return df
 
 
-vectorizer = TfidfVectorizer(stop_words = 'english', max_features = 3000)
-x = vectorizer.fit_transform(df['speech'])
-y = df['party']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 26, stratify = y)
+if __name__ == "__main__":
+    df = subset_and_rename('hansard40000.csv')
+    print(df.shape)
+    
+    vectorizer = TfidfVectorizer(stop_words = 'english', max_features = 3000)
+    x = vectorizer.fit_transform(df['speech'])
+    y = df['party']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 26, stratify = y)
+
