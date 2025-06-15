bf7c1e6968a461aec119b1608c50db5cb84c263f
edwardslydia987 <edwardslydia987@gmail.com>
2025-06-15 13:48:50 +0000

    removing rows not in speech class and with speeches > 1000  chars, and printing shape for Q1a iii and iv

--- PartTwo.py
+++ PartTwo.py
@@ -2,7 +2,13 @@ import pandas as pd
 df = pd.read_csv('hansard40000.csv')
 df['party']  = df['party'].replace('Labour (Co-op)', 'Labour')
 df = df[df.party != 'Speaker']
+
 common_party = df['party'].value_counts().nlargest(4)
 df = df[df['party'].isin(common_party.index)]
-print(common_party)
-print(df)
+
+df = df[df.speech_class == 'Speech']
+
+df = df[df['speech'].str.len() < 1000]
+
+
+print(df.shape)
