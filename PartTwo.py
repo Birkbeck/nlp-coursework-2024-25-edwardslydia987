b6b7dc0f05c06be7c04ceff40a5af8e948983dcb
edwardslydia987 <edwardslydia987@gmail.com>
2025-06-15 17:21:12 +0000

    training random forest and SVM for Q2c

--- PartTwo.py
+++ PartTwo.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score, classification_report
 
 def subset_and_rename(file):
     df = pd.read_csv(file)
@@ -27,4 +31,20 @@ if __name__ == "__main__":
     y = df['party']
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 26, stratify = y)
+
+    rf_classifier = RandomForestClassifier(n_estimators = 300)
+    rf_classifier.fit(x_train, y_train)
+    rf_predictions = rf_classifier.predict(x_test)
+    rf_f1_score = f1_score(y_test, rf_predictions, average = 'macro')
+    rf_report = classification_report(y_test, rf_predictions)
+    print(rf_f1_score)
+    print(rf_report)
+
+    svm_classifier = SVC(kernel = 'linear')
+    svm_classifier.fit(x_train, y_train)
+    svm_predictions = svm_classifier.predict(x_test)
+    svm_f1_score = f1_score(y_test, svm_predictions, average = 'macro')
+    svm_report = classification_report(y_test, svm_predictions)
+    print(svm_f1_score)
+    print(svm_report)
 
